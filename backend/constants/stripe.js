0665d988849d7413f83892f10cefa642696c0d4b
tiptop96 <31468763+tiptop96@users.noreply.github.com>
2017-10-28 15:02:15 +0200

    Update stripe.js

--- backend/constants/stripe.js
+++ backend/constants/stripe.js
@@ -2,7 +2,7 @@ const configureStripe = require('stripe');
 
 const STRIPE_SECRET_KEY = process.env.NODE_ENV === 'production'
     ? '<LIVE_KEY>'
-    : 'sk_test_kfmw4zWNKMPKGQltG2uxjYLt';
+    : '<TEST_KEY>';
 
 const stripe = configureStripe(STRIPE_SECRET_KEY);
 
